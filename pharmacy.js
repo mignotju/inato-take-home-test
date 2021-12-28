ed38803c59fed4448c13b964189a0df6d8375750
Justine Mignot <mignot.justine@gmail.com>
2021-12-28 16:52:32 +0100

    :recycle: (pharmacy.js) updateBenefitValue: refacto Magic Pill case

--- pharmacy.js
+++ pharmacy.js
@@ -12,14 +12,15 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
+      if (this.drugs[i].name === "Magic Pill") {
+        continue;
+      }
       if (
         this.drugs[i].name != "Herbal Tea" &&
         this.drugs[i].name != "Fervex"
       ) {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
         }
       } else {
         if (this.drugs[i].benefit < 50) {
@@ -38,16 +39,12 @@ export class Pharmacy {
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
+      this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       if (this.drugs[i].expiresIn < 0) {
         if (this.drugs[i].name != "Herbal Tea") {
           if (this.drugs[i].name != "Fervex") {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
+              this.drugs[i].benefit = this.drugs[i].benefit - 1;
             }
           } else {
             this.drugs[i].benefit =
