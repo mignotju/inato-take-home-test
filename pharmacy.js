66982035a4691fbbfbfaa53558cadb399fb85be5
Justine Mignot <mignot.justine@gmail.com>
2021-12-29 15:51:28 +0100

    :recycle: (pharmacy.js) updateBenefitValue: refacto Fervex case

--- pharmacy.js
+++ pharmacy.js
@@ -27,36 +27,35 @@ export class Pharmacy {
         }
         continue;
       }
-
-      if (this.drugs[i].name != "Fervex") {
-        if (this.drugs[i].benefit > 0) {
-          this.drugs[i].benefit = this.drugs[i].benefit - 1;
-        }
-      } else {
+      if (this.drugs[i].name === "Fervex") {
         if (this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
+          if (this.drugs[i].expiresIn < 11) {
+            if (this.drugs[i].benefit < 50) {
+              this.drugs[i].benefit = this.drugs[i].benefit + 1;
             }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
+          }
+          if (this.drugs[i].expiresIn < 6) {
+            if (this.drugs[i].benefit < 50) {
+              this.drugs[i].benefit = this.drugs[i].benefit + 1;
             }
           }
         }
+        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+        if (this.drugs[i].expiresIn < 0) {
+          this.drugs[i].benefit = this.drugs[i].benefit - this.drugs[i].benefit;
+        }
+        continue;
       }
+
+      if (this.drugs[i].benefit > 0) {
+        this.drugs[i].benefit = this.drugs[i].benefit - 1;
+      }
+
       this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Fervex") {
-          if (this.drugs[i].benefit > 0) {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        } else {
-          this.drugs[i].benefit = this.drugs[i].benefit - this.drugs[i].benefit;
+        if (this.drugs[i].benefit > 0) {
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
         }
       }
     }
