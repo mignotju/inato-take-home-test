3123bb410853b4b247549889eccbab05f29e80fe
Justine Mignot <mignot.justine@gmail.com>
2021-12-29 15:36:02 +0100

    :recycle: (pharmacy.js) updateBenefitValue: refacto Herbal tea case

--- pharmacy.js
+++ pharmacy.js
@@ -15,10 +15,20 @@ export class Pharmacy {
       if (this.drugs[i].name === "Magic Pill") {
         continue;
       }
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
+      if (this.drugs[i].name === "Herbal Tea") {
+        if (this.drugs[i].benefit < 50) {
+          this.drugs[i].benefit = this.drugs[i].benefit + 1;
+        }
+        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+        if (this.drugs[i].expiresIn < 0) {
+          if (this.drugs[i].benefit < 50) {
+            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          }
+        }
+        continue;
+      }
+
+      if (this.drugs[i].name != "Fervex") {
         if (this.drugs[i].benefit > 0) {
           this.drugs[i].benefit = this.drugs[i].benefit - 1;
         }
@@ -41,19 +51,12 @@ export class Pharmacy {
       }
       this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              this.drugs[i].benefit = this.drugs[i].benefit - 1;
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
+        if (this.drugs[i].name != "Fervex") {
+          if (this.drugs[i].benefit > 0) {
+            this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
+          this.drugs[i].benefit = this.drugs[i].benefit - this.drugs[i].benefit;
         }
       }
     }
