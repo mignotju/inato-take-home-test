5947a7b3fd041e905121666dddf5aeeb2fa176a4
Justine Mignot <mignot.justine@gmail.com>
2021-12-30 12:15:45 +0100

    :white_check_mark: (pharmacy.js) add missing tests for benefit limit values

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -107,6 +107,12 @@ describe("[Pharmacy] updateBenefitValue", () => {
         new Pharmacy([new Drug("Herbal Tea", 5, 50)]).updateBenefitValue()
       ).toEqual([new Drug("Herbal Tea", 4, 50)]);
     });
+
+    it("should increase the benefit to 50 and decrease expiresIn by 1 when benefit >= 48 and expiresIn < 0", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", -5, 49)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -6, 50)]);
+    });
   });
 
   describe("Fervex", () => {
@@ -151,6 +157,24 @@ describe("[Pharmacy] updateBenefitValue", () => {
         new Pharmacy([new Drug("Fervex", -3, 20)]).updateBenefitValue()
       ).toEqual([new Drug("Fervex", -4, 0)]);
     });
+
+    it("should not increase the benefit by 1 and decrease expiresIn by 1 when benefit = 50 expiresIn > 10", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 15, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 14, 50)]);
+    });
+
+    it("should increase benefit to 50 when benefit >= 48 and expiresIn <= 10 and >5", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 9, 49)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 8, 50)]);
+    });
+
+    it("should increase benefit to 50 when benefit >= 47 and expiresIn <= 5 and >0", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 4, 48)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 3, 50)]);
+    });
   });
 
   describe("Dafalgan", () => {
@@ -170,6 +194,18 @@ describe("[Pharmacy] updateBenefitValue", () => {
       expect(
         new Pharmacy([new Drug("Dafalgan", -2, 8)]).updateBenefitValue()
       ).toEqual([new Drug("Dafalgan", -3, 4)]);
+    });
+
+    it("should decrease the benefit to 0 and expiresIn by 1 when benefit <= 2 and expiresIn >= 0", () => {
+      expect(
+        new Pharmacy([new Drug("Dafalgan", 15, 1)]).updateBenefitValue()
+      ).toEqual([new Drug("Dafalgan", 14, 0)]);
+    });
+
+    it("should decrease the benefit to 0 and expiresIn by 1 when benefit is <= 4 and expiresIn < 0", () => {
+      expect(
+        new Pharmacy([new Drug("Dafalgan", -15, 4)]).updateBenefitValue()
+      ).toEqual([new Drug("Dafalgan", -16, 0)]);
     });
   });
 });
